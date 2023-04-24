73cd6bc18319f7ef6e9e3d8fa9b8e4d8d4588184
asrafulsow <aisowrav11@gmail.com>
2023-04-24 20:12:59 +0600

    blog-heading

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>This is blog website</h1>
-      <h1>This is blog website</h1>
-      <h1>This is blog website</h1>
-      <h1>This is blog website</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, fuga.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, fuga.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, fuga.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, fuga.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, fuga.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, fuga.</p>
     </div>
   );
 }
