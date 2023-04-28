ffa437146180d1ac730ccb868d39efc8ecc1561a
asrafulsow <aisowrav11@gmail.com>
2023-04-28 19:41:18 +0600

    Home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
 import Home from './components/Home/Home/Home';
+import About from './components/Home/About/About';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route to="/" element={<Home />} />
+          <Route path='/' element={<Home />} />
+          <Route path='about' element={<About />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h2>This is blog home page</h2>
+            <h1 className='text-4xl text-red-950'>This is Home Page</h1>
         </div>
     );
 };
