3cde0f0309b5300f235f9a1f9e01e38077d78e1c
asrafulsow <aisowrav11@gmail.com>
2023-04-28 21:37:18 +0600

    d

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home/Home';
 import About from './components/Home/About/About';
+import Navigation from './components/Home/Shared/Navigation/Navigation';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navigation />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='about' element={<About />} />
--- src/components/Home/Shared/Navigation/Navigation.js
+++ src/components/Home/Shared/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navigation = () => {
     return (
         <div>
-            <h1>THis is Navigation</h1>
+
         </div>
     );
 };
