07d6eb9d6ec8e4f33d6d2be99ac2c8152c49433a
asrafulsow <aisowrav11@gmail.com>
2023-05-17 21:21:50 +0600

    n

--- src/components/Home/Shared/Footer/Footer.js
+++ src/components/Home/Shared/Footer/Footer.js
@@ -6,6 +6,7 @@ const Footer = () => {
             <div class="grid grid-cols-4 gap-4">
                 <div>01</div>
                 <div>09</div>
+                <div>09</div>
             </div>
 
         </div>
