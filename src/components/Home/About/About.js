5c5a138da699706dc3808ff040066e07833f1252
asrafulsow <aisowrav11@gmail.com>
2023-05-20 22:08:56 +0600

    s

--- src/components/Home/About/About.js
+++ src/components/Home/About/About.js
@@ -4,6 +4,9 @@ const About = () => {
     return (
         <div>
             <h2 className='text-4xl font-bold mt-5'>This is About us page</h2>
+            <h2 className='text-4xl font-bold mt-5'>This is About us page</h2>
+            <h2 className='text-4xl font-bold mt-5'>This is About us page</h2>
+            <h2 className='text-4xl font-bold mt-5'>This is About us page</h2>
         </div>
     );
 };
